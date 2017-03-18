586ac01952e556d6c7396b407e87d76aac43b9df
Michael <michael@cybellum.com>
2017-03-19 01:32:41 +0200

    Added PATH_Combine

--- DoubleAgent/Path.c
+++ DoubleAgent/Path.c
@@ -57,3 +57,49 @@ lbl_cleanup:
 	/* Returns status */
 	return eStatus;
 }
+
+DOUBLEAGENT_STATUS PATH_Combine(IN PCWSTR pcwszPath1, IN PCWSTR pcwszPath2, OUT PWSTR *ppwszCombined)
+{
+	DOUBLEAGENT_STATUS eStatus = DOUBLEAGENT_STATUS_INVALID_VALUE;
+	PWSTR pwszCombinedLocal = NULL;
+
+	/* Validates the parameters */
+	if ((NULL == pcwszPath1) || (NULL == pcwszPath2) || (NULL == ppwszCombined))
+	{
+		DOUBLEAGENT_SET(eStatus, DOUBLEAGENT_STATUS_DOUBLEAGENT_PATH_COMBINE_INVALID_PARAMS);
+		goto lbl_cleanup;
+	}
+
+	/* Allocates the combined path */
+	pwszCombinedLocal = HeapAlloc(GetProcessHeap(), HEAP_ZERO_MEMORY, MAX_PATH);
+	if (NULL == pwszCombinedLocal)
+	{
+		DOUBLEAGENT_SET(eStatus, DOUBLEAGENT_STATUS_DOUBLEAGENT_PATH_COMBINE_HEAPALLOC_FAILED);
+		goto lbl_cleanup;
+	}
+
+	/* Combines the paths */
+	if (NULL == PathCombineW(pwszCombinedLocal, pcwszPath1, pcwszPath2))
+	{
+		DOUBLEAGENT_SET(eStatus, DOUBLEAGENT_STATUS_DOUBLEAGENT_PATH_COMBINE_PATHCOMBINEW_FAILED);
+		goto lbl_cleanup;
+	}
+
+	/* Sets the received parameters */
+	*ppwszCombined = pwszCombinedLocal;
+	pwszCombinedLocal = NULL;
+
+	/* Succeeded */
+	DOUBLEAGENT_SET(eStatus, DOUBLEAGENT_STATUS_SUCCESS);
+
+lbl_cleanup:
+	/* Frees the combined path */
+	if (NULL != pwszCombinedLocal)
+	{
+		(VOID)HeapFree(GetProcessHeap(), 0, pwszCombinedLocal);
+		pwszCombinedLocal = NULL;
+	}
+
+	/* Returns status */
+	return eStatus;
+}
--- DoubleAgent/Path.h
+++ DoubleAgent/Path.h
@@ -10,3 +10,8 @@
  * The directory path must be freed via HeapFree
  */
 DOUBLEAGENT_STATUS PATH_GetDirectory(IN PCWSTR pcwszFilePath, OUT PWSTR *ppwszDirPath);
+/*
+ * Receives two file paths and combines them into a single valid path
+ * The combined path must be freed via HeapFree
+ */
+DOUBLEAGENT_STATUS PATH_Combine(IN PCWSTR pcwszPath1, IN PCWSTR pcwszPath2, OUT PWSTR *ppwszCombined);
